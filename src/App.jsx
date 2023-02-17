4e3c86f4516438877017d4ce5fb7fbc21af6b717
Newo <scerep529@gmail.com>
2023-02-17 16:38:13 +0600

    ...

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  let count = 0;
+  const countPlus = () => {
+    return count + 1;
+  }
+
+  return (
+    <div className="App">
+    </div>
+  );
+}
+
+export default App;
